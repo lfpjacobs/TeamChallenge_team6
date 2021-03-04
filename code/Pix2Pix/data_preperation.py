lfpjacobs/TeamChallenge_team6

commit 234b6fc853704ac9c1bda311ef8e7bf3f78b001f

Efficient shuffling implemented

code/Pix2Pix/data_preperation.py
@@ -176,6 +176,10 @@ def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.8, 
     if not os.path.isdir((os.path.join("..", "..", "data", "preprocessed", "brain_extraction"))):
         os.mkdir((os.path.join("..", "..", "data", "preprocessed", "brain_extraction")))
 
+    # initialize data
+    src_array = np.zeros((len(subjectDirs)*crop_size[2], 256, 256))
+    tar_array = np.zeros((len(subjectDirs)*crop_size[2], 256, 256))
+    j = 0
     # Iteratively loop over subjects and extract data
     for subject_n in range(len(subjectDirs)):
 
@@ -207,20 +211,11 @@ def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.8, 
 
         # Add individual slices to image lists 
         for slice_i in range(crop_size[2]):
-            src_images.append(img_src_crop[:,:,slice_i])
-            tar_images.append(img_tar_crop[:,:,slice_i])
-        
+            src_array[j] = img_src_crop[:,:,slice_i]
+            tar_array[j] = img_tar_crop[:,:,slice_i]
+            j += 1
+            
         print("Completed")
-    
-    # Shuffle slices (coherently) and then store them in numpy arrays
-    indices = list(range(len(src_images)))
-    random.shuffle(indices)
-
-    src_images_shuffled = [src_images[i] for i in indices]
-    tar_images_shuffled = [tar_images[i] for i in indices]
-
-    src_array = np.array(src_images_shuffled)
-    tar_array = np.array(tar_images_shuffled)
 
     if len(src_images) > 0:
         print(f"\nCompleted data extraction!\nFound a total of {len(src_images)} slices\n")

code/Pix2Pix/train_util.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import tensorflow as tf
+import random
 from numpy import load
 from numpy import zeros
 from numpy import ones
@@ -53,8 +54,8 @@ class Logger(object):
     
     def log_images(self, tag, images, step):
         with self.writer.as_default():
-              tf.summary.image(tag, np.asarray(images), max_outputs=len(images), step=step)
-              self.writer.flush()
+            tf.summary.image(tag, np.asarray(images), max_outputs=len(images), step=step)
+            self.writer.flush()
               
 # generate samples and save as a plot and save the model
 def summarize_performance(step, g_model, dataset, modelsDir, logger, run, n_samples=3):
@@ -108,16 +109,23 @@ def train(d_model, g_model, gan_model, dataset, n_epochs=100, n_batch=1):
     # manually enumerate epochs
     for i in range(n_steps):
         if (i) % (bat_per_epo) == 0: # per epoch "refresh" training set with new augmentations 
-            A_list, B_list = [], []
+            # initialize augmented dataset
+            A = np.zeros((len(trainA)*n_aug, 256, 256, 1))
+            B = np.zeros((len(trainA)*n_aug, 256, 256, 1))
+            # for shuffling of all slices
+            rand_i = list(range(len(trainA)*n_aug)) 
+            random.shuffle(rand_i) # unique list of random indices
+            k = 0
+            
             for n in range(n_aug):
                 for j in range(len(trainA)):
-                    # Augment every image randomely per epoch (prevents memory problems since you're overwriting)
+                    # Augment every image randomely per epoch
                     trainA_aug, trainB_aug = augment(trainA[j], trainB[j])
-                    A_list.append(trainA_aug)
-                    B_list.append(trainB_aug)
-            
-        # TODO: Implement coherent shuffling
-        dataset_aug = [np.array(A_list), np.array(B_list)]
+                    A[rand_i[k]] = trainA_aug
+                    B[rand_i[k]] = trainB_aug
+                    k += 1
+                    
+            dataset_aug = [A, B] # all trainingdata (day4 and day1)
         
         # select a batch of real samples
         [X_realA, X_realB], y_real = generate_real_samples(dataset_aug, n_batch, n_patch)

commit fd2fd45a420d6257e3c2faf10a708520e5d708d0

Small bugfix Fixed bug introduced in commit 234b6fc

code/Pix2Pix/data_preperation.py
@@ -149,9 +149,6 @@ def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.8, 
     Additionally, the model is trained on 2D slices of the data. TODO: Might want to do this differently
     Also, only DWI-B0 images are taken into account.
     """
-
-    # Predefine lists for the source and target images
-    src_images, tar_images = [], []
     
     # Define required path and (if applicable) make a division for train/test sets
     subjectDirs = glob(os.path.join(datadir, "rat*"))
@@ -217,9 +214,9 @@ def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.8, 
             
         print("Completed")
 
-    if len(src_images) > 0:
-        print(f"\nCompleted data extraction!\nFound a total of {len(src_images)} slices\n")
-        if not len(src_images) == len(tar_images) : raise ValueError("There aren't as many source as target images. Check for missing files.")
+    if np.shape(src_array)[2] > 0:
+        print(f"\nCompleted data extraction!\nFound a total of {np.shape(src_array)[2]} slices\n")
+        if not np.shape(src_array)[2] == np.shape(tar_array)[2] : raise ValueError("There aren't as many source as target images. Check for missing files.")
     else:
         raise ValueError("The selected data directory doesn't contain any properly formatted data")
     