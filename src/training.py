803b32ec3f009f4437d0ee8808b35221d7cae32b
sjors <s.h.a.verschuren@student.tue.nl>
2021-03-30 15:15:58 +0200

    Improved training - Implemented proper epochs - Made verbose notebook-proof

--- src/training.py
+++ src/training.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 import random
 import math
+from tqdm import tqdm
+import warnings
 from numpy import load
 from numpy import zeros
 from numpy import ones
@@ -13,6 +15,7 @@ from numpy.random import randint
 from augmentation import augment
 from datetime import datetime
 from skimage.metrics import structural_similarity
+from util.general import print_style
 
 # load and prepare training images
 def load_real_samples(filename):
@@ -25,17 +28,28 @@ def load_real_samples(filename):
     X2 = (X2 - 127.5) / 127.5
     return [X1, X2]
 
-# select a batch of random samples, returns images and target
-def generate_real_samples(dataset, n_samples, patch_shape):
-	# unpack dataset
+
+def generate_real_samples(dataset, n_samples, patch_shape, available_idx=None):
+    """
+    Function that selects a batch of random samples from a list of available ones, returns images and target.
+    """
+    # unpack dataset
     trainA, trainB = dataset
-	# choose random instances
-    ix = randint(0, trainA.shape[0], n_samples)
-	# retrieve selected images
-    X1, X2 = trainA[ix], trainB[ix]
-	# generate 'real' class labels (1)
-    y = ones((n_samples, patch_shape, patch_shape, 1))
-    return [X1, X2], y
+
+    # choose random instances
+    if type(available_idx) in [list, np.ndarray]:
+        ix = np.random.randint(0, len(available_idx), n_samples)
+        img_idx = available_idx[ix]
+    else:
+        img_idx = np.random.randint(0, np.shape(trainA)[0], n_samples)
+        ix = None
+
+    # retrieve selected images
+    X1, X2 = trainA[img_idx], trainB[img_idx]
+    # generate 'real' class labels (1)
+    y = np.ones((n_samples, patch_shape, patch_shape, 1))
+
+    return [X1, X2], y, ix
 
 # generate a batch of images, returns images and targets
 def generate_fake_samples(g_model, samples, patch_shape):
@@ -63,12 +77,12 @@ class Logger(object):
 # generate samples and save as a plot and save the model
 def summarize_performance(step, g_model, dataset_train, dataset_val, modelsDir, logger, run, n_samples=3):
 	# select a sample of input images
-    [X_realA_train, X_realB_train], _ = generate_real_samples(dataset_train, n_samples, 1)
+    [X_realA_train, X_realB_train], _, _ = generate_real_samples(dataset_train, n_samples, 1)
 	# generate a batch of fake samples
     X_fakeB_train, _ = generate_fake_samples(g_model, X_realA_train, 1)
 
     # select a sample of input images
-    [X_realA_val, X_realB_val], _ = generate_real_samples(dataset_val, n_samples, 1)
+    [X_realA_val, X_realB_val], _, _ = generate_real_samples(dataset_val, n_samples, 1)
 	# generate a batch of fake samples
     X_fakeB_val, _ = generate_fake_samples(g_model, X_realA_val, 1)
 	
@@ -83,7 +97,7 @@ def summarize_performance(step, g_model, dataset_train, dataset_val, modelsDir, 
 
 def check_ssim(g_model, dataset, n_samples=3):
     # select a sample of input images
-    [X_realA, X_realB], _ = generate_real_samples(dataset, n_samples, 1)
+    [X_realA, X_realB], _, _ = generate_real_samples(dataset, n_samples, 1)
 	# generate a batch of fake samples
     X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
     
@@ -123,7 +137,7 @@ def split_train_val(dataset, split_factor=0.8):
     return dataset_train, dataset_val
 
 # train pix2pix model
-def train(d_model, g_model, gan_model, dataset_train, n_epochs=100, n_batch=1):    
+def train(d_model, g_model, gan_model, dataset_train, n_epochs=100, n_batch=4):    
     # Extract current time for model/plot save files
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
@@ -170,68 +184,84 @@ def train(d_model, g_model, gan_model, dataset_train, n_epochs=100, n_batch=1):
     logger_val = Logger(os.path.join(logsDir, "ssim_val"))
     
     # manually enumerate epochs
-    for i in range(n_steps):
-        if (i) % (bat_per_epo) == 0: # per epoch "refresh" training set with new augmentations
-            print("Performing data augmentation... ", end="", flush=True) 
-            # initialize augmented dataset
-            A = np.zeros((len(trainA_ori)*n_aug, 256, 256, 1))
-            B = np.zeros((len(trainA_ori)*n_aug, 256, 256, 1))
-            # for shuffling of all slices
-            rand_i = list(range(len(trainA_ori)*n_aug)) 
-            random.shuffle(rand_i) # unique list of random indices
-            k = 0
-            
-            for n in range(n_aug):
-                for j in range(len(trainA_ori)):
-                    # Augment every image randomely per epoch
-                    trainA_aug, trainB_aug = augment(trainA_ori[j], trainB_ori[j])
-                    A[rand_i[k]] = trainA_aug
-                    B[rand_i[k]] = trainB_aug
-                    k += 1
+    i = 0
 
-                    # Write images for debugging purposes
-                    # filename_realA = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_realA.png")
-                    # filename_A = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_A.png")
-                    # filename_B = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_B.png")
-                    # debug_img_realA = Image.fromarray((255*trainA_ori[j].reshape((256, 256))).astype(np.uint8))
-                    # debug_imgA = Image.fromarray((255*trainA_aug.reshape((256, 256))).astype(np.uint8))
-                    # debug_imgB = Image.fromarray((255*trainB_aug.reshape((256, 256))).astype(np.uint8))
-                    # debug_img_realA.save(filename_realA)
-                    # debug_imgA.save(filename_A)
-                    # debug_imgB.save(filename_B)
-                    
-            dataset_aug = [A, B] # all trainingdata (day4 and day1)
-            print("Completed")
+    for epoch in range(n_epochs):
+        print("\n"+print_style.BOLD+f"Epoch {epoch+1}/{n_epochs}:"+print_style.END)
 
-        # select a batch of real samples
-        [X_realA, X_realB], y_real = generate_real_samples(dataset_aug, n_batch, n_patch)
-        # generate a batch of fake samples
-        X_fakeB, y_fake = generate_fake_samples(g_model, X_realA, n_patch)
-        # update discriminator for real samples
-        d_loss1 = d_model.train_on_batch([X_realA, X_realB], y_real)
-        # update discriminator for generated samples
-        d_loss2 = d_model.train_on_batch([X_realA, X_fakeB], y_fake)
-        # update the generator
-        g_loss, _, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB, X_realB])
-        # summarize performance
-        print('>%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i+1, d_loss1, d_loss2, g_loss))
+        # Create list of available indices --> Images can't be used twice in the same epoch.
+        available_idx = np.array(range(np.shape(dataset_train)[1]))
 
-        # summarize model performance and store models
-        if (i+1) % (bat_per_epo) == 0:
-            summarize_performance(i, g_model, dataset_train, dataset_val, modelsDir, logger_im, current_time)
+        # For each epoch, create a new augmented dataset
+        print("Performing data augmentation... ", end="", flush=True) 
+        # initialize augmented dataset
+        A = np.zeros((len(trainA_ori)*n_aug, 256, 256, 1))
+        B = np.zeros((len(trainA_ori)*n_aug, 256, 256, 1))
+        # for shuffling of all slices
+        rand_i = list(range(len(trainA_ori)*n_aug)) 
+        random.shuffle(rand_i) # unique list of random indices
+        k = 0
         
-        # Store losses (tensorboard) 
-        if (i+1) % (bat_per_epo // 50) == 0:
-            logger_g.log_scalar('run_{}'.format(current_time), g_loss, i)
-            logger_d1.log_scalar('run_{}'.format(current_time), d_loss1, i)
-            logger_d2.log_scalar('run_{}'.format(current_time), d_loss2, i)  
+        for n in range(n_aug):
+            for j in range(len(trainA_ori)):
+                # Augment every image randomely per epoch
+                trainA_aug, trainB_aug = augment(trainA_ori[j], trainB_ori[j])
+                A[rand_i[k]] = trainA_aug
+                B[rand_i[k]] = trainB_aug
+                k += 1
 
-        # Store similarities (tensorboard)
-        if (i+1) % (bat_per_epo // 20) == 0:
-            similarity_train = check_ssim(g_model, dataset_train, 1)
-            similarity_val = check_ssim(g_model, dataset_val, 1) # should probably select one validation slice instead
+                # Write images for debugging purposes
+                # filename_realA = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_realA.png")
+                # filename_A = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_A.png")
+                # filename_B = os.path.join("data", "aug_debug", str(i)+"_"+str(n)+"_"+str(j)+"_B.png")
+                # debug_img_realA = Image.fromarray((255*trainA_ori[j].reshape((256, 256))).astype(np.uint8))
+                # debug_imgA = Image.fromarray((255*trainA_aug.reshape((256, 256))).astype(np.uint8))
+                # debug_imgB = Image.fromarray((255*trainB_aug.reshape((256, 256))).astype(np.uint8))
+                # debug_img_realA.save(filename_realA)
+                # debug_imgA.save(filename_A)
+                # debug_imgB.save(filename_B)
+                
+        dataset_aug = [A, B] # all trainingdata (day4 and day1)
+        print("Completed")
+
+        for batch in tqdm(range(bat_per_epo), ascii=True):           
+
+            # select a batch of real samples
+            [X_realA, X_realB], y_real, used_idx = generate_real_samples(dataset_aug, n_batch, n_patch, available_idx)
+
+            np.delete(available_idx, used_idx)
+
+            # generate a batch of fake samples
+            X_fakeB, y_fake = generate_fake_samples(g_model, X_realA, n_patch)
+
+            # Perform actual training
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+
+                # update discriminator for real samples
+                d_loss1 = d_model.train_on_batch([X_realA, X_realB], y_real)
+                # update discriminator for generated samples
+                d_loss2 = d_model.train_on_batch([X_realA, X_fakeB], y_fake)
+                # update the generator
+                g_loss, _, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB, X_realB])               
             
-            logger_train.log_scalar('run_{}'.format(current_time), similarity_train, i) # higher is better (more similar)
-            logger_val.log_scalar('run_{}'.format(current_time), similarity_val, i)
+            # Store losses (tensorboard) 
+            if (i+1) % (bat_per_epo // 50) == 0:
+                logger_g.log_scalar('run_{}'.format(current_time), g_loss, i)
+                logger_d1.log_scalar('run_{}'.format(current_time), d_loss1, i)
+                logger_d2.log_scalar('run_{}'.format(current_time), d_loss2, i)  
+
+            # Store similarities (tensorboard)
+            if (i+1) % (bat_per_epo // 20) == 0:
+                similarity_train = check_ssim(g_model, dataset_train, 1)
+                similarity_val = check_ssim(g_model, dataset_val, 1) # should probably select one validation slice instead
+                
+                logger_train.log_scalar('run_{}'.format(current_time), similarity_train, i) # higher is better (more similar)
+                logger_val.log_scalar('run_{}'.format(current_time), similarity_val, i)
+
+            i += 1
+        
+        print('>Losses: d1[%.3f] d2[%.3f] g[%.3f]' % (d_loss1, d_loss2, g_loss))
+        summarize_performance(i, g_model, dataset_train, dataset_val, modelsDir, logger_im, current_time)
     
     return current_time
--- util/tf_session.py
+++ util/tf_session.py
@@ -1,9 +1,11 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 import tensorflow as tf
 
 def setup_tf_session():
   """
   Function that fixes memory problems for gpu runs
-  """
+  """ 
 
   gpus = tf.config.experimental.list_physical_devices('GPU')
   if gpus:
