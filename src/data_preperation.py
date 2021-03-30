Attempt at image normalisation Training process won't work on standardized images somehow. Trying this fix. Otherwise we'll go back to histogram equalization

src/data_preperation.py
@@ -167,13 +167,26 @@ def histogram_equalization(img, number_bins=256):
 
 def standardize_img(img):
     """
-    This function standardizes the brain-part of the image.
-    The background is set to the minimal brain value - 1 std.
+    This function standardizes an image
     """
 
     img_std = (img - np.mean(img)) / np.std(img)
 
     return img_std
+
+
+def normalize_img(img, range=(0, 1)):
+    """
+    This function normalizes an image.
+    It sets a minimum and a certain standard deviation. 
+    This solution should resolve some issues (standardize method) with low pixel values around important structures, 
+    while background etc. had very high pixel values.
+    """
+    min, std = range
+
+    img_norm = ((img - np.min(img) + min) * std )/ np.std(img)
+
+    return img_norm
 
 
 def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.7, random_seed=1234):
@@ -240,8 +253,8 @@ def data_prep(datadir, split_dataset=False, train_or_test="", split_factor=0.7, 
         # Now, crop the images to remove unnecessary empty space
         [img_src_crop, img_tar_crop] = crop_brain(img_src_pad, img_tar_pad, os.path.split(subjectDir)[-1], importance_map, crop_size)
 
-        img_src_std = standardize_img(img_src_crop)
-        img_tar_std = standardize_img(img_tar_crop)
+        img_src_std = normalize_img(img_src_crop)
+        img_tar_std = normalize_img(img_tar_crop)
 
         # Add individual slices to image lists 
         for slice_i in range(crop_size[2]):

src/training.py
@@ -92,13 +92,15 @@ def summarize_performance(step, g_model, dataset_train, dataset_val, modelsDir, 
     print('>Saved model: {}s'.format(filename))
     
     # Generate images to be displayed
-    realA_train = (X_realA_train[0] - np.min(X_realA_train[0])) / (np.max(X_realA_train[0]) - np.min(X_realA_train[0]))
-    fakeB_train = (X_fakeB_train[0] - np.min(X_fakeB_train[0])) / (np.max(X_fakeB_train[0]) - np.min(X_fakeB_train[0]))
-    realB_train = (X_realB_train[0] - np.min(X_realB_train[0])) / (np.max(X_realB_train[0]) - np.min(X_realB_train[0]))
+    scale_train = (np.max(X_realA_train[0]) - np.min(X_realA_train[0]) + np.max(X_realB_train[0]) - np.min(X_realB_train[0])) / 2
+    realA_train = (X_realA_train[0] - np.min(X_realA_train[0])) / scale_train
+    fakeB_train = (X_fakeB_train[0] - np.min(X_fakeB_train[0])) / scale_train
+    realB_train = (X_realB_train[0] - np.min(X_realB_train[0])) / scale_train
 
-    realA_val = (X_realA_val[0] - np.min(X_realA_val[0])) / (np.max(X_realA_val[0]) - np.min(X_realA_val[0]))
-    fakeB_val = (X_fakeB_val[0] - np.min(X_fakeB_val[0])) / (np.max(X_fakeB_val[0]) - np.min(X_fakeB_val[0]))
-    realB_val = (X_realB_val[0] - np.min(X_realB_val[0])) / (np.max(X_realB_val[0]) - np.min(X_realB_val[0]))
+    scale_val = (np.max(X_realA_val[0]) - np.min(X_realA_val[0]) + np.max(X_realB_val[0]) - np.min(X_realB_val[0])) / 2
+    realA_val = (X_realA_val[0] - np.min(X_realA_val[0])) / scale_val
+    fakeB_val = (X_fakeB_val[0] - np.min(X_fakeB_val[0])) / scale_val
+    realB_val = (X_realB_val[0] - np.min(X_realB_val[0])) / scale_val
 
     # log images
     logger.log_images('run_{}_step{}_train'.format(run, step), [realA_train, fakeB_train, realB_train], step)